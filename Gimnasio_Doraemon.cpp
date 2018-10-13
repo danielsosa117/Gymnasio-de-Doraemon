4dbfb8afad7f7ea38a29a9f900831df9e66b6f57
Daniel Fernando Sosa Suárez <danielsosa117@javerianacali.edu.co>
2018-10-13 11:33:14 -0500

    ACTUALIZACION

--- Gimnasio_Doraemon.cpp
+++ Gimnasio_Doraemon.cpp
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------//
 | Nombre del Proyecto: Gimnasio De Doraemon.
 | Dia De Inicio: 08-09-2018.
-| Version 4.0.
+| Version 1.0.
 |
 | Integrantes:
 |
@@ -23,30 +23,71 @@
 #include "Zona_Pesas.h"
 #include "Zona_Cardio.h"
 #include "Zona_Humeda.h"
-#include "BasesDatos.h"
 
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <vector>
 #include <iomanip>
-#include <cctype>
-#include <cstdio>
-#include <stdlib.h>
 using namespace std;
-
-/*---------------------------------------------------------------//
-| Nombre y contraseña del administrador, estas van a ser tomadas durante el funcionamiento |	
-//---------------------------------------------------------------*/
-
-const string admin = "rey"; string contrasena = "gimnasio01";
 
 /*---------------------------------------------------------------//
 | Variables constantes para la introduccion de vocales con tilde |	
 //---------------------------------------------------------------*/
 
 const int  a = 160, o = 162, i = 161, e = 130, u = 163;
+
+//Vector con los datos de los usuarios
+vector< vector<string> > Usuarios;
+vector<string> datos;
+
+void llenarVectorUsuarios(){
+
+	ifstream archivo("usuarios.txt");
+	string linea, dato;
+
+	while(getline(archivo,linea)){
+
+		for (int i = 0; i < linea.size(); i++){
+
+			if(linea[i] != '-'){
+				dato.push_back(linea[i]);
+			}
+			else{
+				datos.push_back(dato);
+				dato.clear();
+			}
+
+			if(i == linea.size() - 1){
+				datos.push_back(dato);
+				dato.clear();
+			}
+		}
+		Usuarios.push_back(datos);
+		datos.clear();
+	}
+	archivo.close();
+}
+
+bool buscarPersona(string nombre){
+
+	for(int i = 0; i < Usuarios.size();i++){
+		if(Usuarios[i][1].compare(nombre) == 0)
+			return true;
+	}
+	return false;
+}
+
+void motrarUsuarios(){
+
+	cout << "Cargo" << '\t'<< right << "Nombre" << '\t' << "Edad" << '\t' << "G" << (char)e << "nero" << '\t' << endl;
+	for(int i = 0; i < Usuarios.size(); i++){
+		cout << Usuarios[i][0] << '\t' << Usuarios[i][1] << '\t' << Usuarios[i][2] << '\t' << Usuarios[i][3] << '\t' << endl;
+	}
+}
+
 
 /*---------------------------------------------------------------//
 | Funcion de conteo de tiempo de espera
@@ -71,16 +112,18 @@ void delay(int secs) {
 | el usuario
 |	
 //---------------------------------------------------------------*/
-
 int main(){
 
-	string nombre, maquina, opcionesEntrada, opcionIngreso, opcionRegistrado;
-	int edad, contador;
-	Archivos archivos;
+	//Manejo de Entradas
+	string nombre, maquina, opcionRegistrado;
+	int edad, contador,  opcionesEntrada, opcionIngreso; 
+
+	llenarVectorUsuarios();
 
 	/*---------------------------------------------------------------//
 	| Creacion del Gimnacio desde cero
 	//---------------------------------------------------------------*/
+
 	Gimnasio *gym = new Gimnasio(0,0,0,0,0);
 
 	do{
@@ -92,15 +135,32 @@ int main(){
 
 		cout<<"::--:: Bienvenidos Al Gimnasio de Doraemon .:::."<<endl;
 		cout<<"   Favor escoge una opci"<<(char)o<<"n para la simulaci"<<(char)o<<"n"<<endl;
-		cout<<"1) Usuario."<<endl;
+		cout<<"1) Usuario"<<endl;
 		cout<<"2) Administrador."<<endl;
 		cout<<"3) Salir."<<endl;
 		cout<<endl;
-
 		cout<<"Opci"<<(char)o<<"n: ";
 		cin>>opcionesEntrada;
-
+		try{
+			if(opcionesEntrada < 1) throw 1;
+			if(opcionesEntrada > 3) throw 2;
+		}
+		catch(int error){
+			system("cls");
+			cout<< "Valor ingresado invalido. "<< endl;
+			if(error == 1){
+				cout<<"Este numero esta por debajo del rango"<<endl;
+				delay(2);
+				system("PAUSE");
+			}
+			if(error == 2){
+				cout<<"Este numero esta por ensima del rango"<<endl;
+				delay(2);
+				system("PAUSE");
+			}
+		}
 		system("cls");
+		
 		//Termina menu de entrada
 
 		//Efecto de espera
@@ -116,7 +176,7 @@ int main(){
 		| portero y contador cada uno con sus respecivas opciones
 		//---------------------------------------------------------------*/
 
-		if(opcionesEntrada.compare("1") == 0){
+		if(opcionesEntrada==1){
 			
 			//Bienvenida
 			cout<<"Bienvenido a la opci"<<(char)o<<"n de Usuario"<<endl;
@@ -135,6 +195,24 @@ int main(){
 				cout<<endl;
 				cout<<"Opci"<<(char)o<<"n: ";
 				cin>>opcionIngreso;
+				try{
+					if(opcionIngreso < 1) throw 1;
+					if(opcionIngreso > 2) throw 2;
+				}
+				catch(int error){
+					system("cls");
+					cout<< "Valor ingresado invalido. "<< endl;
+					if(error == 1){
+						cout<<"Este numero esta por debajo del rango"<<endl;
+						delay(2);
+						system("PAUSE");
+					}
+					if(error == 2){
+						cout<<"Este numero esta por ensima del rango"<<endl;
+						delay(2);
+						system("PAUSE");
+					}
+				}
 				system("cls");
 				//Termina menu ingreso
 
@@ -148,33 +226,16 @@ int main(){
 				cout<<"Un momento por favor..."<<endl;
 				delay(1);
 				system("cls");
-				//Termina efecto de espera
-
-				/*---------------------------------------------------------------//
-				| Exepciones basicas de introduccion de texto
-				//---------------------------------------------------------------*/
-				if(opcionIngreso.compare("1") != 0 && opcionIngreso.compare("2") != 0){
-
-					cout<<"La opci"<<(char)o<<"n "<<opcionIngreso<<" no existe."<<endl;
-					delay(1);
-				}
-
-				/*---------------------------------------------------------------//
-				| Por el momento no tenemos la implementacion de los usuarios ya
-				| ya que esta requiere una gran cantidad de tipos y la estamos planteando
-				| en un documento diferente
-				//---------------------------------------------------------------*/
-	
-				if(opcionIngreso.compare("1") == 0){
+				
+				//Opcion ingreso 1
+				if(opcionIngreso == 1){
 
 					system("cls");
-					cout<< "Esta funci" << (char)o << "n se llevara a cabo en la siguiente entrega." << endl;
-					system("PAUSE");
-
-
+					motrarUsuarios();
+					system("pause");
 				}
 
-			}while(opcionIngreso.compare("2") != 0);
+			}while(opcionIngreso != 2);
 		}
 		//Termina opcion entrada 1
 
@@ -185,8 +246,7 @@ int main(){
 		| de capacidad la contratacion y despido de clientes, cambios de capacidad,
 		| inicializacion total del gymnasion o cerrar totalmente el gym
 		//---------------------------------------------------------------*/
-		
-		if(opcionesEntrada.compare("2") == 0){
+		if(opcionesEntrada==2){
 
 			//Bienvenida
 			cout<<"Bienvenido a la opci"<<(char)o<<"n de Administrador"<<endl;
@@ -200,20 +260,36 @@ int main(){
 				system("cls");
 
 				//Menu ingreso
-
 				cout<<"..::.. Opci"<<(char)o<<"n del Administrador..::.."<<endl<<endl;
 				cout<<"1) Inagurar El Gimnasio de Doraemon."<<endl;
 				cout<<"2) Cambiar el Estado del Gimnasio."<<endl;
-				cout<<"3) Cambiar el Estado de las Zonas."<<endl; 
+				cout<<"3) Cambiar el Estado de las Zonas."<<endl;
 				cout<<"4) Cambiar la capacidad de las Zonas."<<endl;
 				cout<<"5) Realizar contrataciones."<<endl;
 				cout<<"6) Despedir a clientes."<<endl;
-				cout<<"7) Mostrar datos de empleados"<<endl;
-				cout<<"8) Mostrar datos del Gimnasio"<<endl;
+				cout<<"7) Mostrar datos del Gimnasio"<<endl;
 				cout<<"0) Salir."<<endl;
 				cout<<endl;
 				cout<<"Opci"<<(char)o<<"n: ";
 				cin>>opcionIngreso;
+				try{
+					if(opcionIngreso < 0) throw 1;
+					if(opcionIngreso > 7) throw 2;
+				}
+				catch(int error){
+					system("cls");
+					cout<< "Valor ingresado invalido. "<< endl;
+					if(error == 1){
+						cout<<"Este numero esta por debajo del rango"<<endl;
+						delay(2);
+						system("PAUSE");
+					}
+					if(error == 2){
+						cout<<"Este numero esta por ensima del rango"<<endl;
+						delay(2);
+						system("PAUSE");
+					}
+				}
 				system("cls");
 				//Termina menu ingreso
 
@@ -229,97 +305,52 @@ int main(){
 				system("cls");
 				//Termina efecto de espera
 
+				
+
+				//Opcion ingreso 1
 				/*---------------------------------------------------------------//
 				| Inicializacion del Gymnasio desde cero 						 |	
 				//---------------------------------------------------------------*/
 
-				if(opcionIngreso.compare("1") == 0){
-					string Zona_Pesas, Zona_Humeda, Zona_Cardio, Recepcion;
-					int Gym;
-					bool aceptado = false;
+				if(opcionIngreso == 1){
+					int Zona_Pesas, Zona_Humeda, Zona_Cardio, Recepcion, Gym;
+
 					cout<<"Bienvenido a la inaguracion del Gimnasio de Doraemon"<<endl;
 					delay(2);
 					system("cls");
 					cout<<"..::..Por favor ingresa los datos..::.."<<endl;
-					do{
-						cout<<"Capacidad total de personas de la Recepcion: ";
-						cin>>Recepcion;
-						for (int i = 0; i < Recepcion.size(); i++){
-							if (isalpha(Recepcion[i])){
-								aceptado = false;
-								break;
-							}
-
-							if (i == Recepcion.size() - 1)
-								aceptado = true;
-						}
-						system("cls");
-					} while(aceptado == false);
-					gym->set_CapMaxima( "recepcion", atoi(Recepcion.c_str()));
-					Gym += atoi(Recepcion.c_str());
-					aceptado = false;
-					do{
-						system("cls");
-						cout<<"Capacidad total de personas de la Zona Pesas: ";
-						cin>>Zona_Pesas;
-						for (int i = 0; i < Zona_Pesas.size(); i++){
-							if (isalpha(Zona_Pesas[i])){
-								aceptado = false;
-								break;
-							}
-
-							if (i == Zona_Pesas.size() - 1)
-								aceptado = true;
-						}
-					} while(aceptado == false);
-					gym->set_CapMaxima( "zona_pesas", atoi(Zona_Pesas.c_str()));
-					Gym += atoi(Zona_Pesas.c_str());
-					aceptado = false;
-					do{
-						system("cls");
-						cout<<"Capacidad total de personas de la Zona Cardio: ";
-						cin>>Zona_Cardio;
-						for (int i = 0; i < Zona_Cardio.size(); i++){
-							if (isalpha(Zona_Cardio[i])){
-								aceptado = false;
-								break;
-							}
-
-							if (i == Zona_Cardio.size() - 1)
-								aceptado = true;
-						}
-					} while(aceptado == false);
-					gym->set_CapMaxima( "zona_cardio", atoi(Zona_Cardio.c_str()));
-					Gym += atoi(Zona_Cardio.c_str());
-					aceptado = false;
-					do{
-						system("cls");
-						cout<<"Capacidad total de personas de la Zona Humeda: ";
-						cin>>Zona_Humeda;
-						for (int i = 0; i < Zona_Humeda.size(); i++){
-							if (isalpha(Zona_Humeda[i])){
-								aceptado = false;
-								break;
-							}
-
-							if (i == Zona_Humeda.size() - 1)
-								aceptado = true;
-						}
-					} while(aceptado == false);
-					gym->set_CapMaxima( "zona_humeda", atoi(Zona_Humeda.c_str()));
-					Gym += atoi(Zona_Humeda.c_str());
-					aceptado = false;
+					cout<<"Capacidad total de personas de la Recepcion: ";
+					cin>>Recepcion;
+					gym->set_CapMaxima( "recepcion", Recepcion);
+					Gym += Recepcion;
+					system("cls");
+					cout<<"Capacidad total de personas de la Zona Pesas"<<endl;
+					cout<<"Capacidad total de personas de la Zona Pesas: ";
+					cin>>Zona_Pesas;
+					gym->set_CapMaxima( "zona_pesas", Zona_Pesas);
+					Gym += Zona_Pesas;
+					system("cls");
+					cout<<"Capacidad total de personas de la Zona Cardio"<<endl;
+					cout<<"Capacidad total de personas de la Zona Cardio: ";
+					cin>>Zona_Cardio;
+					gym->set_CapMaxima( "zona_cardio", Zona_Cardio);
+					Gym += Zona_Cardio;
+					system("cls");
+					cout<<"Capacidad total de personas de la Zona Humeda: ";
+					cin>>Zona_Humeda;
+					gym->set_CapMaxima( "zona_humeda", Zona_Humeda);
+					Gym += Zona_Humeda;
 					system("cls");
 					gym->set_CapMaximaGym(Gym);
-					cout<<"..::..Gracias por su colaboraci"<< (char)o << "n..::.."<<endl;
-					delay(1);				
+					cout<<"..::..Gracias por su colaboracion..::.."<<endl;
+					delay(1);					
 				}
 
 				/*---------------------------------------------------------------//
-				| Cambio del estado general del Gimnasio de doraemon				 |	
+				| Cambio de 					 |	
 				//---------------------------------------------------------------*/
 
-				else if(opcionIngreso.compare("2") == 0){
+				else if(opcionIngreso == 2){
 					int estado;
 
 					cout<<"..::.. Bienvenido al modulo de cambio de estado de Gimnasio ..::.."<<endl;
@@ -334,12 +365,9 @@ int main(){
 					delay(1);
 				}
 
-				/*---------------------------------------------------------------//
-				| Cambio del estado general del Gimnasio de doraemon				 |	
-				//---------------------------------------------------------------*/
-
-				else if(opcionIngreso.compare("3") == 0){
-					string zona, estado;
+				else if(opcionIngreso== 3){
+					int estado;
+					string zona;
 
 					cout<<"..::.. Bienvenido al modulo de modificacion del estado de las Zonas ..::.."<<endl;
 					cout<<"   Favor escoge un estado y una zona "<<endl;
@@ -347,129 +375,64 @@ int main(){
 					cout<<"2) En mantenimiento."<<endl;
 					cout<<"0) Cerrado."<<endl<<endl;
 					cout<<"Zona: ";
-					cin.ignore();
-					getline(cin, zona);
+					cin>>zona;
 					cout<<"Estado: ";
-					getline(cin, estado);
-					gym->set_estZonas(zona, atoi(estado.c_str()));
+					cin>>estado;
+					gym->set_estZonas(zona, estado);
 					system("cls");
 					cout<<"Gracias por su colaboracion"<<endl;
 					delay(1);
 				}
-				/*-------------------------------------------------------------------------------//
-				| Cambio de capacidad por zonas unitariamente, es nesesario que escribas
-				| Zona Humeda por ejemplo para que el programa te saque un resultado satisfactorio				 |	
-				//-------------------------------------------------------------------------------*/
 
-				else if(opcionIngreso.compare("4") == 0){
-					string capacidad;
-					bool aceptado = false;
+				else if(opcionIngreso == 4){
+					int capacidad;
 					string zona;
-					do{
-						system("cls");
-						cout<<"..::.. Bienvenido al modulo de modificacion de capacidad maxima de Zonas ..::.."<<endl;
-						cout<<"Favor escoge una cantidad y una zona "<<endl;
-						cout << left << "C" << (char)o << setw(9) <<"digo" << "Zonas" << endl << endl;
-						cout << left << setw(11) << "1" << "Pesas" << endl;
-						cout << left << setw(11) << "2" << "Cardio" << endl;
-						cout << left << setw(11) << "3" << "Humeda" << endl;
-						cout << left << setw(11) << "4" << "Recepci" << (char)o << "n" << endl;
-					
 
-						cout<<"Zona: ";
-						cin >> zona;
-						if (zona.size() != 1)
-							aceptado = false;
-						else if(isalpha(zona[0]))
-							aceptado = false;
-						else if(atoi(zona.c_str()) < 1 || atoi(zona.c_str()) > 4)
-								aceptado = false;
-						else
-							aceptado = true;
-
-					} while(aceptado == false);
-					aceptado = false;
-					if (zona.compare("1") == 0)
-						zona = "Zona Pesas";
-					else if (zona.compare("2") == 0)
-						zona = "Zona Cardio";
-					else if (zona.compare("3") == 0)
-						zona = "Zona Humeda";
-					else
-						zona = "Recepcion";
-
-					do{		
-
-						cout<<"Capacidad: ";
-						cin.ignore();
-						cin>>capacidad;
-						for (int i = 0; i < capacidad.size(); i++){
-							if (isalpha(capacidad[i])){
-								aceptado = false;
-								break;
-							}
-
-							if (i == capacidad.size() - 1)
-								aceptado = true;
-						}
-					} while(aceptado == false);
-					aceptado = false;
-					gym->set_CapMaxima(zona,atoi(capacidad.c_str()));
+					cout<<"..::.. Bienvenido al modulo de modificacion de capacidad maxima de Zonas ..::.."<<endl;
+					cout<<"   Favor escoge una cantidad y una zona "<<endl;
+					cout<<"Zona: ";
+					cin>>zona;
+					cout<<"Capacidad: ";
+					cin>>capacidad;
+					gym->set_CapMaxima(zona,capacidad);
 					system("cls");
 					cout<<"Gracias por su colaboracion"<<endl;
 					delay(1);
 				}
-				/*---------------------------------------------------------------//
-				| Contratacion de empleados por zona es nesesario introducir los 
-				| los nombres determinados por la zona (recepcionista, empleado_servico)	
-				//---------------------------------------------------------------*/
 
-				else if(opcionIngreso.compare("5") == 0){
-					string zona;
-
-					cout << ".::. Bienvenido al modulo de contratacion de empleados ..:::.."<< endl;
-					cout << ".::. ingresa la zona en la cual quieres realizar la contratacion ..:::.."<< endl;
+				else if(opcionIngreso== 5){
+					string zona, cargo;
+					system("cls");
+					cout<<"..::.. Bienvenido al modulo de contrataciones ..::.."<<endl;
+					cout<<"Por favor ingresa la siguiente informacion"<<endl;
 					cout<<"Zona: ";
-					cin.ignore();
-					getline(cin, zona);
-					cin.ignore();
-					gym->contratar_empleado_zona(zona);
-					system("PAUSE");
-
+					cin>> zona;
+					cout<<"Cargo: ";
+					cin>>cargo;
+					gym->contratar_empleado_zona(zona, cargo);
+					cout<<"Gracias por su colaboracion"<<endl;
+					delay(1);
 				}
-				else if(opcionIngreso.compare("7") == 0){
-					string zona;
-					cout << ".::. Bienvenido al modulo de informacion de empleados ..:::.."<< endl;
-					cout << ".::. ingresa la zona en la que se encuentra el empleado ..:::.."<< endl;
-					cout<<"Zona: ";
-					cin.ignore();
-					getline(cin, zona);
-					gym->mostrar_datos_empleados_zona(zona);
-					system("PAUSE");
-				}
-				/*---------------------------------------------------------------//
-				| Muestra datos generales del gimnasio y sus zonas				 |	
-				//---------------------------------------------------------------*/
 
-				else if(opcionIngreso.compare("8") == 0){
+
+
+				else if(opcionIngreso == 7){
 					gym->mostrar_estado_general();
 				}
 
-			}while(opcionIngreso.compare("0") != 0);
+			}while(opcionIngreso == 0);
 
 		}
-		//Termina op
-		//terminacion entrada 2
-		if(opcionesEntrada.compare("3")){
+
+		if(opcionesEntrada ==3){
 			//Despedida
 			cout<<"Bienvenido al men"<<(char)u<<" principal."<<endl;
 			delay(1);
 			system("cls");
 			//Termina Despedida
 		}
-	
 		
-	}while(opcionesEntrada.compare("3") != 0);
+	}while(opcionesEntrada!=3);
 	
 	//Despedida
 	system("cls");
